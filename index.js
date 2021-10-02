784c14a1defdfa206d1e761f854d5ab072323088
cdnss <32522595+cdnss@users.noreply.github.com>
2021-10-02 23:32:06 +0700

    update index.js

--- index.js
+++ index.js
@@ -5,50 +5,51 @@ const puppeteer = require('puppeteer-core');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<a href="/v/https://d21.run/">jjjjjj</a>');
+ res.send('<a href="/v/https://d21.run/">jjjjjj</a>');
 });
 
 app.get('/v/*', async (req, res) => {
-  const urll = req.url;
-  if (!urll) {
-    return res.send('please provide url');
-  }
-let url = urll.replace('/v/', '');
-  try {
-    const browser = await puppeteer.launch(
-process.env.NODE_ENV === 'production'
-      ? {
-      
+ const urll = req.url;
+ if (!urll) {
+  return res.send('please provide url');
+ }
+ let url = urll.replace('/v/', '');
+ try {
+  const browser = await puppeteer.launch(
+   process.env.NODE_ENV === 'production'
+   ? {
 
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: false, //chrome.headless,
-        ignoreHTTPSErrors: true,
-dumpio:false,
 
-    } 
-: {}
-);
-  
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: false, //chrome.headless,
+    ignoreHTTPSErrors: true,
+    dumpio: false,
+
+   }: {}
+  );
+  browser.defaultBrowserContext();
   const page = await browser.newPage();
-  page.setUserAgent('Opera/9.80 (J2ME/MIDP; Opera Mini/5.1.21214/28.2725; U; ru) Presto/2.8.119 Version/11.10');
+
   await page.setExtraHTTPHeaders({
-    'upgrade-insecure-requests': '1'
+   'upgrade-insecure-requests': '1'
   });
 
-    await page.goto(url, {waitUntil: 'networkidle0', timeout:30000});
-    const pageContent = await page.content();
-    console.log(`Response first 200 chars from ${url} : ${pageContent.substring(0, 200)}`);
-    await browser.close();
-    
-    res.send(pageContent);
-  } catch (err) {
-    console.log(`Error while fetching ${url} `, err);
-    res.send(`Error fetching ${url}`);
-  }  
+  await page.goto(url, {
+   waitUntil: 'networkidle0', timeout: 30000
+  });
+  const pageContent = await page.content();
+  console.log(`Response first 200 chars from ${url} : ${pageContent.substring(0, 200)}`);
+  await browser.close();
+
+  res.send(pageContent);
+ } catch (err) {
+  console.log(`Error while fetching ${url} `, err);
+  res.send(`Error fetching ${url}`);
+ }
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+ console.log(`Listening on port ${PORT}`);
 });
